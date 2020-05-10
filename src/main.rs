18853ae51d4424543487d426101c59088567105d
denebola213 <dene@st-albireo.net>
2020-05-10 21:04:25 +0900

    learned until the 12th day

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ fn main() {
     // fizz_buzz4()
     // fizz_buzz5()
     // fizz_buzz6()
-    fizz_buzz7()
+    // fizz_buzz7()
+    // fizz_buzz10();
+    // fizz_buzz11();
+    fizz_buzz12();
 }
 
 fn fizz_buzz1() {
@@ -96,3 +99,72 @@ fn fizz_buzz7() {
         println!("{}", s);
     }
 }
+
+// 8-9 day skipped
+
+fn fizz_buzz10() {
+    fn fz(x: i32) -> String {
+        match (x % 3, x % 5) {
+        (0, 0) => format!("FizzBuzz"),
+        (0, _) => format!("Fizz"),
+        (_, 0) => format!("Buzz"),
+            _  => x.to_string(),
+        }
+    };
+
+    let res =  (1 ..= 100).map(fz).collect::<Vec<_>>().join("\n");
+
+    println!("{}", res);
+}
+
+fn fizz_buzz11() {
+    fn fz<T>(x: T, div_a: T, div_b: T, zero: T) -> String
+    where T: Rem<T, Output=T> + Eq + Copy + ToString {
+        match (x % div_a == zero , x % div_b == zero) {
+            (true, true) => format!("FizzBuzz"),
+            (true, _) => format!("Fizz"),
+            (_, true) => format!("Buzz"),
+                _  => x.to_string(),
+        }
+    };
+
+    (1 ..= 100).map(|x: u32| fz(x, 3,5, 0)).for_each(|x| println!("{}", x));
+}
+
+struct FizzBuzz<T> {
+    div_a: T,
+    div_b: T,
+    zero: T,
+}
+
+impl<T> FizzBuzz<T> {
+    fn new(div_a: T, div_b: T, zero: T) -> Self {
+        FizzBuzz {div_a, div_b, zero}
+    }
+}
+
+trait ToFzStr<T> {
+    fn to_str(&self, x: T) -> String;
+}
+
+use std::ops::Rem;
+fn common_fz_str<T>(x: T, div_a: T, div_b: T, zero: T) -> String
+where T: Rem<T, Output=T> + Eq + Copy + ToString {
+    match (x % div_a == zero , x % div_b == zero) {
+        (true, true) => format!("FizzBuzz"),
+        (true, _) => format!("Fizz"),
+        (_, true) => format!("Buzz"),
+            _  => x.to_string(),
+    }
+}
+
+impl ToFzStr<i32> for FizzBuzz<i32> {
+    fn to_str(&self, x: i32) -> String {
+        common_fz_str(x, self.div_a, self.div_b, self.zero)
+    }
+}
+
+fn fizz_buzz12() {
+    (1 ..= 100).map(|x| FizzBuzz::new(3, 5, 0).to_str(x)).for_each(|x| println!("{}", x))
+}
+
